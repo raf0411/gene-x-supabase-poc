f2ae7496e24b20496e52a82491e5fbd8774debdb
Raffi <raffidarmawan0411@gmail.com>
2025-03-14 00:28:21 +0700

    first commit

---
+++ app/src/main/java/kalbe/corp/genexsupabasepoc/MainActivity.kt
@@ -0,0 +1,33 @@
+package kalbe.corp.genexsupabasepoc
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import kalbe.corp.genexsupabasepoc.ui.theme.GeneXSupabasePOCTheme
+
+class MainActivity : ComponentActivity() {
+    @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            GeneXSupabasePOCTheme {
+                Box(Modifier.fillMaxSize().padding(top = 64.dp)){
+                    Text("Hello World!")
+                }
+            }
+        }
+    }
+}
+
